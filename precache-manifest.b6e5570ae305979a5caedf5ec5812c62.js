b8b9f76db0f5de13acdc9cc4bfc8384d90731884
whiz <ngenovic4@gmail.com>
2020-08-04 20:03:56 +0300

    Updates

---
+++ precache-manifest.b6e5570ae305979a5caedf5ec5812c62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962a45aed5bab1cda0f6d314472a4b4c",
+    "url": "/movies-React-Front-End/index.html"
+  },
+  {
+    "revision": "99def6891292726e89ed",
+    "url": "/movies-React-Front-End/static/css/main.ffcf2704.chunk.css"
+  },
+  {
+    "revision": "8042ebb8a10a4d7b0365",
+    "url": "/movies-React-Front-End/static/js/2.a4095756.chunk.js"
+  },
+  {
+    "revision": "de50617b6dc34b3ca124c4a4cded6991",
+    "url": "/movies-React-Front-End/static/js/2.a4095756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99def6891292726e89ed",
+    "url": "/movies-React-Front-End/static/js/main.9f09a12e.chunk.js"
+  },
+  {
+    "revision": "de2638532041705fcb93",
+    "url": "/movies-React-Front-End/static/js/runtime-main.2aab1449.js"
+  }
+]);
